2e475bf93ee9b0a81b096f54ee83bf6bb534ac47
Fengyun LI <lify@pku.edu.cn>
2015-03-09 11:24:03 +0800

    Mon Mar  9 11:24:03 CST 2015

---
+++ bes3/script/setroot534.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#===============================================================================
+#   FILENAME:         setroot534.sh
+#   AUTHOR:           Fengyun Li,lfypku@gmail.com
+#   CREATED:          2014年12月18日 01时42分17秒 CST
+#   DESCRIPTION: 
+#===============================================================================
+
+source /afs/ihep.ac.cn/soft/atlas/ROOT/v5.34.01/bin/thisroot.sh
