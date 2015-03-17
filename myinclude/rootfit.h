53e9b16a106344f99e39e159f1a5a6f6dfcf9ee7
Fengyun LI <lify@pku.edu.cn>
2015-03-17 16:04:35 +0800

    Tue Mar 17 16:04:35 CST 2015

--- myinclude/rootfit.h
+++ myinclude/rootfit.h
@@ -1,10 +1,10 @@
 #ifndef ROOTFIT_H
 #define ROOTFIT_H 1
 
-#include "/workfs/bes/lify/package/include/root/LCPP.h"
-#include "/workfs/bes/lify/package/include/root/LRooHead.h"
-#include "/workfs/bes/lify/package/include/root/LRooFit.h"
-#include "/workfs/bes/lify/package/include/root/LPlot.h"
+#include "/workfs/bes/lify/myinclude/root/LCPP.h"
+#include "/workfs/bes/lify/myinclude/root/LRooHead.h"
+#include "/workfs/bes/lify/myinclude/root/LRooFit.h"
+#include "/workfs/bes/lify/myinclude/root/LPlot.h"
 
 
 #endif
--- myinclude/rootult.h
+++ myinclude/rootult.h
@@ -1,13 +1,13 @@
 #ifndef ROOTULT_H
 #define ROOTULT_H 1
 
-#include "/workfs/bes/lify/package/include/root/LCPP.h"
-#include "/workfs/bes/lify/package/include/root/LRooHead.h"
-#include "/workfs/bes/lify/package/include/root/LRooFit.h"
-#include "/workfs/bes/lify/package/include/root/LPlot.h"
+#include "/workfs/bes/lify/myinclude/root/LCPP.h"
+#include "/workfs/bes/lify/myinclude/root/LRooHead.h"
+#include "/workfs/bes/lify/myinclude/root/LRooFit.h"
+#include "/workfs/bes/lify/myinclude/root/LPlot.h"
 
-#include "/workfs/bes/lify/package/include/class/LCkEnv.h"
-#include "/workfs/bes/lify/package/include/func/LMath.h"
+#include "/workfs/bes/lify/myinclude/class/LCkEnv.h"
+#include "/workfs/bes/lify/myinclude/func/LMath.h"
 
 #endif
 
