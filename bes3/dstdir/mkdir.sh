a9583ca25d87a9f1c006d63a4960d285d6ba869a
Fengyun LI <lify@pku.edu.cn>
2015-05-26 04:00:03 +0800

    Tue May 26 04:00:03 CST 2015

--- bes3/dstdir/mkdir.sh
+++ bes3/dstdir/mkdir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MODIFY=1
-DSTPATH="/besfs/groups/higgs/users/lify/664p01/dst/etapee/gpipi/pipipi"
+DSTPATH="/scratchfs/bes/lify/664/mc/etapee/gampipi/pipipi/dst"
 CPWD=$(pwd -P)
 OUTFILE=path.txt
 test -f path.txt && rm -rf path.txt
