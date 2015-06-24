90c90d7be001768f1daf1b9fb70d0dcd4bec40a9
Fengyun LI <lify@pku.edu.cn>
2015-06-25 04:00:01 +0800

    Thu Jun 25 04:00:01 CST 2015

--- myinclude/shell/alias.sh
+++ myinclude/shell/alias.sh
@@ -7,6 +7,7 @@ alias grep='grep --color=auto'
 alias du1='du -h --max-depth 1'
 alias du2='du -h --max-depth 2'
 alias qs="qstat -u $USER"
+alias lt='ls -lt'
 #PS1="\[\e[0;34m\][\[\e[m\]\[\e[0;32m\]\u\[\e[m\]\[\e[0;32m\]@\[\e[m\]\[\e[0;32m\]\h\[\e[m\] \[\e[0;36m\]\A\[\e[m\] \[\e[0;31m\]\W\[\e[m\]\[\e[0;34m\]]\[\e[m\]\$ "
 #export PS1="\[\e[0;34m\][\[\e[m\]\[\e[0;32m\]\u\[\e[m\]\[\e[0;32m\]@\[\e[m\]\[\e[0;32m\]\h\[\e[m\] \[\e[0;36m\]\A\[\e[m\] \[\e[0;35m\]\W\[\e[m\]\[\e[0;34m\]]\[\e[m\]\[\e[0;34m\]\$\[\e[m\] \[\e[0;31m\]"
 NOHOST=`echo $HOSTNAME|cut -c 6-8`
