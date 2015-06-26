bdf43be3cbb64828ce31e933f2b2edd51d7b6a25
Fengyun LI <lify@pku.edu.cn>
2015-06-27 04:00:01 +0800

    Sat Jun 27 04:00:01 CST 2015

--- myinclude/root/LRooStyle.h
+++ myinclude/root/LRooStyle.h
@@ -32,7 +32,7 @@ void cdmystyle(){
 	myStyle->SetStatX(0.90);
 	myStyle->SetStatY(0.90);
 
-	myStyle->SetNdivisions(505,"x");
+	myStyle->SetNdivisions(510,"x");
 	myStyle->SetNdivisions(510,"y");
 	myStyle->SetTickLength(.03,"xyz");
 
@@ -55,7 +55,7 @@ void cdmystyle(){
 	myStyle->SetLabelSize(0.04,"xyz");
 	myStyle->SetTitleSize(0.06,"xyz");
 	myStyle->SetLabelOffset(0.005);
-	myStyle->SetTitleOffset(0.7,"x");
+	myStyle->SetTitleOffset(0.6,"x");
 	myStyle->SetTitleOffset(0.9,"y");
 	myStyle->SetTitleFont(22);
 	myStyle->SetLabelFont(22);
