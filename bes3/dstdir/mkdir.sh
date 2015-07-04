2a3d9558c0ed2aea2595b43dd781b5d1a7215cdd
Fengyun LI <lify@pku.edu.cn>
2015-07-05 04:00:02 +0800

    Sun Jul  5 04:00:01 CST 2015

--- bes3/dstdir/mkdir.sh
+++ bes3/dstdir/mkdir.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MODIFY=1
 #DSTPATH="/scratchfs/bes/lify/664/mc/etapee/gampipi/pipipi/dst"
-DSTPATH="/scratchfs/bes/lify/664/mc/etapee/gampipi/12gampipiee/dst"
+DSTPATH="/scratchfs/bes/lify/664/mc/etapu/gampipi/sig/09/dst"
 
 CPWD=$(pwd -P)
 OUTFILE=path.txt
---
+++ mytest/geant4_soft.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt-get install libxerces-c-dev libmotif-dev libglw1-mesa-dev inventor-dev opticalraytracer libpythia8-dev pythia8-doc-html pythia8-doc-worksheet pythia8-examples
