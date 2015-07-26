ba1518b385e5ec0317f7f72269ce9d98c7b2d005
Fengyun LI <lify@pku.edu.cn>
2015-07-27 04:00:01 +0800

    Mon Jul 27 04:00:01 CST 2015

--- bes3/script/tool/cal2.C
+++ bes3/script/tool/cal2.C
@@ -3,12 +3,12 @@
 void cal2()
 {
 double a,f,sigma1,sigma2,mu1,mu2,mcmean,fitmean1,fitmean2;
-f=1.6484e-01;
-sigma1= 1.4481e-02;
-sigma2= 4.4099e-03;
-mcmean=0.95778;
-fitmean1=9.5648e-01;
-fitmean2=9.5782e-01;
+f=0.2246;
+sigma1= 0.01040;
+sigma2= 0.004398;
+mcmean=0.1349766;
+fitmean1=0.133473;
+fitmean2=0.134532;
 mu1=fabs(fitmean1-mcmean);
 mu2=fabs(fitmean2-mcmean);
 double y=sqrt(f*sigma1*sigma1+(1-f)*sigma2*sigma2+f*(1-f)*(mu1-mu2)*(mu1-mu2));
