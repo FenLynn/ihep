36be9e59092e83f233c163e32f1ec708597a6979
Fengyun LI <lify@pku.edu.cn>
2015-11-04 04:00:01 +0800

    Wed Nov  4 04:00:01 CST 2015

--- bes3/dstdir/mkdst.sh
+++ bes3/dstdir/mkdst.sh
@@ -2,7 +2,7 @@
 MODIFY=1
 #DSTPATH="/scratchfs/bes/lify/664/mc/etapee/gampipi/pipipi/dst"
 #DSTPATH="/scratchfs/bes/lify/664p03/mc/etapee/gampipi/gametap/dst"
-DSTPATH="/scratchfs/bes/lify/664p03/mc/etapee/etapipi/jpsietagampipi/dst"
+DSTPATH="/scratchfs/bes/lify/664/mc/etapu/gampipi/sig/HELAMP/dst"
 
 FILE_TYPR=""
 
