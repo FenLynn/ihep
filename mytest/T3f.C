3441e3447f1a889c41ddf617cafec3379cf0bff1
Fengyun LI <lify@pku.edu.cn>
2016-01-19 04:00:02 +0800

    Tue Jan 19 04:00:02 CST 2016

---
+++ mytest/T3f.C
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+#include "/workfs/bes/lify/myinclude/rootall.h"
+
+void t3f(){
+	TH3F *a= new TH3F("","",10,-10,10,10,10,20,10,30,40);
+	for(int i=0;i<10
+}
